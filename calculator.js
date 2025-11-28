da47311a75fcdcea486ebb9ad549b707cd554326
AlderSan <gentr023@gmail.com>
2025-11-27 18:58:10 -0600

    Add algorithm comments

---
+++ calculator.js
@@ -0,0 +1,39 @@
+//the stored numbers and operators
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+//function - on num button press
+//if operator === ''
+//add num to num1 (as string for multi digits)
+//if operator !== ''
+//add num to num2 (as string for multi digits)
+//run display function
+
+//function - on operator button press
+//if num2 === ''
+//change operator to button value
+//if num2 !== ''
+//send current num1, num2, operator to calculate function
+//then change operator to button value
+//run display function
+
+//function - on equals button press
+//if num2 !== ''
+//process calculation
+//else do nothing
+//run display function
+
+//calculate function
+//takes num1, num2, operator values
+//object map - passed in the values, operator is key, value is function to perform the math
+//display result as num1
+//set num2, operator variables to ''
+
+//display function
+//if num2 !== ''
+//display num2 on screen
+//else if operator !== ''
+//display operator on screen
+//else display num1 on screen
+
